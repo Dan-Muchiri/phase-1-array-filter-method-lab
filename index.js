d2391f960b1d2178e794aa6987d0a200720178f4
Dan Muchiri <dan.muchiri@student.moringaschool.com>
2023-12-22 20:10:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr, driver){
+    const dr = driver.toUpperCase();
+    const driverName = arr.filter(element => element.toUpperCase() === dr)
+    return (driverName);
+}
+
+function fuzzyMatch(arr, letters){
+    const driverName = arr.filter(element => element.startsWith(letters));
+    return (driverName);
+}
+
+function matchName(arr, driver){
+    const driverName = arr.filter(element => element.name === driver) 
+    return (driverName);
+}
